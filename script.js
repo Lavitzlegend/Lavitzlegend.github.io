26f7d51ae5a21bbdc52de11855a591a575d1add2
jordan-dyar <jordan.dyar218@gmail.com>
2021-01-11 10:50:04 -0600

    cleaned up check() functionality and added timer to start next round after check() is true

--- script.js
+++ script.js
@@ -3,6 +3,29 @@ const simonArr = [];
 let playerArr = [];
 
 document.querySelector("#begin").addEventListener("click", function(e) {
+    nextRound();
+})
+
+document.querySelector("#green").addEventListener("click", function(e) {
+    playerArr.push("green");
+    check(simonArr, playerArr);
+})
+document.querySelector("#red").addEventListener("click", function(e) {
+    playerArr.push("red");
+    check(simonArr, playerArr);
+})
+document.querySelector("#yellow").addEventListener("click", function(e) {
+    playerArr.push("yellow");
+    check(simonArr, playerArr);
+})
+document.querySelector("#blue").addEventListener("click", function(e) {
+    playerArr.push("blue");
+    check(simonArr, playerArr);
+})
+
+let nextRound = () => {
+    document.querySelector("#begin").textContent = "playing...";
+    document.querySelector("#begin").style.backgroundColor = "white";
     document.querySelector("h1").classList.add("hideshow");
     playerArr = [];
     let color = Math.floor(Math.random() * 4) + 1;
@@ -31,48 +54,19 @@ document.querySelector("#begin").addEventListener("click", function(e) {
         }
     }, 1000);
     console.log(simonArr);
-})
-
-
-document.querySelector("#green").addEventListener("click", function(e) {
-    // console.log(playerArr);
-    playerArr.push("green");
-    check(simonArr, playerArr);
-    document.querySelector("#begin").style.color = "lightgreen";
-})
-document.querySelector("#red").addEventListener("click", function(e) {
-    // console.log(playerArr);
-    playerArr.push("red");
-    check(simonArr, playerArr);
-    document.querySelector("#begin").style.color = "lightgreen";
-})
-document.querySelector("#yellow").addEventListener("click", function(e) {
-    // console.log(playerArr);
-    playerArr.push("yellow");
-    check(simonArr, playerArr);
-    document.querySelector("#begin").style.color = "lightgreen";
-})
-document.querySelector("#blue").addEventListener("click", function(e) {
-    // console.log(playerArr);
-    playerArr.push("blue");
-    check(simonArr, playerArr);
-    document.querySelector("#begin").style.color = "lightgreen";
-})
-
-let nextRound = () => {
-    document.querySelector("h1").textContent = "Correct! Press Next Round to continue"
 }
+
 let check = (a, b) => {
     if (a.length === b.length && a.every((v, i) => v === b[i])) { // found an efficient method for comparing 2 arrays at this URL: https://www.30secondsofcode.org/blog/s/javascript-array-comparison
         document.querySelector("h1").textContent = "Correct!";
-        nextRound();
+        document.querySelector("h1").classList.remove("hideshow");
+        let winWait = setTimeout(function() {
+            nextRound();
+        }, 500);
     }
-    else {
+    else if (a.length === b.length && a.every((v, i) => v !== b[i])) {
         document.querySelector("h1").textContent = "Incorrect. Please refresh page to start over"
+        document.querySelector("h1").classList.remove("hideshow");
     }    
 }
 
-document.querySelector("#check").addEventListener("click", function(e) {
-    document.querySelector("h1").classList.remove("hideshow");
-    check(simonArr, playerArr);
-})
